ed7d554e0098bdb9f2de500d921a9d4c113a21b6
almamarie <alouismariea97@gmail.com>
2022-01-01 21:39:31 +0000

    the first commit did not include the files so I git add the views folder and all its subfolders and => configured screenutilinit in the main.dart, created the views directory, created the contactHome.dart file for the contacts homepage and created the contacthomepage.dart for the contacts page

---
+++ lib/views/contactHome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ContactHome extends StatelessWidget {
+  const ContactHome({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
