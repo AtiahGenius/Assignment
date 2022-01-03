5908b013320c18e1dd7f2d25ce6426d7e897f808
almamarie <alouismariea97@gmail.com>
2022-01-03 18:20:32 +0000

    Created the Mobile and Email details

--- lib/views/contactNotifications.dart
+++ lib/views/contactNotifications.dart
@@ -17,7 +17,7 @@ class ContactNotfications extends StatelessWidget {
     return TextStyle(
       fontSize: fontSize.sp,
       fontWeight: fontWeight,
-      color: Colors.grey,
+      color: Color.fromARGB(51, 51, 51, 1),
     );
   }
 
@@ -70,7 +70,117 @@ class ContactNotfications extends StatelessWidget {
                 'Brisbane, Australia',
                 style: DarkStyle(fontSize: 16, fontWeight: FontWeight.w400),
               ),
-            )
+            ),
+            SizedBox(
+              height: 24.h,
+            ),
+            Container(
+              color: const Color.fromRGBO(154, 173, 190, 0.2),
+              height: 60.h,
+              child: Padding(
+                padding: EdgeInsets.only(left: 43.2.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 11.h,
+                        ),
+                        Text(
+                          "Mobile",
+                          style: DarkStyle(
+                              fontSize: 14, fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "+61 491 570 156",
+                          style: StyleLight(
+                              fontSize: 12, fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 20.r,
+                          child: IconButton(
+                            iconSize: 20.sp,
+                            color: Colors.black,
+                            onPressed: () {},
+                            icon: const Icon(Icons.message),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 7.w,
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 20.r,
+                          child: IconButton(
+                            iconSize: 20.sp,
+                            color: Colors.black,
+                            onPressed: () {},
+                            icon: const Icon(Icons.call),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 25.w,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: const Color.fromRGBO(154, 173, 190, 0.2),
+              height: 60.h,
+              child: Padding(
+                padding: EdgeInsets.only(left: 43.2.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 11.h,
+                        ),
+                        Text(
+                          "Email",
+                          style: DarkStyle(
+                              fontSize: 14, fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "danielmortz23@gmail.com",
+                          style: StyleLight(
+                              fontSize: 12, fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 20.r,
+                          child: IconButton(
+                            iconSize: 20.sp,
+                            color: Colors.black,
+                            onPressed: () {},
+                            icon: const Icon(Icons.mail),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 25.w,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
